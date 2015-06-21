5ed38af27afcab28461a447c88f80b227e30ca58
Melissa Noelle <melissamatara@gmail.com>
2015-06-21 16:39:54 -0400

    Serving a public file now woooo.

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var http = require('http');
 var serveStatic = require('serve-static');
+var path = require('path');
 
 var app = express();
 
@@ -10,5 +11,5 @@ server.listen(25660, '0.0.0.0', function() {
   console.error("Shit is fucking running on port 25660.");
 });
 
-
+app.use('/', serveStatic(path.join(__dirname, 'public/'))); // magic?
 
