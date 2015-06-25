cef85ed1375232315e84b422dbe2589e9dd35452
Melissa Noelle <melissamatara@gmail.com>
2015-06-25 19:57:22 -0400

    Added in a websocket connection.

--- server.js
+++ server.js
@@ -2,13 +2,33 @@ var express = require('express');
 var http = require('http');
 var serveStatic = require('serve-static');
 var path = require('path');
+var yawl = require('yawl');
 
 var app = express();
 
 var server = http.createServer(app);
 
 server.listen(25660, '0.0.0.0', function() {
-  console.error("Shit is fucking running on port 25660.");
+  console.error("Running on port 25660.");
+});
+
+var wss = yawl.createServer({
+  server: server,
+  allowTextMessages: true,
+  origin: null,
+});
+
+wss.on('error', function(er) {
+  console.error('Web socket is alllll fucked up: ', er.stack);
+});
+
+wss.on('connection', function(ws) {
+  console.log('someone is joining the party: ');
+
+  ws.sendText(JSON.stringify({
+    name: 'derp',
+    args: Date.now(),
+  }));
 });
 
 app.use('/', serveStatic(path.join(__dirname, 'public/'))); // magic?
