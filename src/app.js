22465996a8d39d2c3d6f194280784ff9fce59a16
Melissa Noelle <madamei@mojolingo.com>
2015-06-29 18:44:47 -0400

    Remove commented out code.

--- src/app.js
+++ src/app.js
@@ -3,7 +3,6 @@ var React = require('react');
 
 var socket = new Socket();
 
-//var DominionClient = React.createFactory(require('./react/DominionClient.jsx'));
 var DominionClient = require('./react/DominionClient.jsx');
 
 var mountNode = document.getElementById("container");
